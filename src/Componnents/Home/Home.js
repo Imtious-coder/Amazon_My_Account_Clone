59913089486732059ca9142806fbf7dce6dd0a49
unknown <iarafat386@gmail.com>
2021-05-23 13:56:29 +0600

    Card 2 and 3 complete

--- src/Componnents/Home/Home.js
+++ src/Componnents/Home/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Home.css';
 import Image1 from './Your_Orders.png';
+import Image2 from './Login_Security.png';
+import Image3 from './Prime.png';
+import Image4 from './GiftCard.png';
+import Image5 from './payment.png';
+import Image6 from './Your_Profile.png';
+import Image7 from './digital_devices.png';
+import Image8 from './Messages.jpg';
+import Image9 from './archived_orders.png';
+import Image10 from './Lists.png';
 
 const Home = () => {
     return (
@@ -14,9 +23,10 @@ const Home = () => {
             {/* Main Part Start */}
             <main className="container">
                 <div className="row">
+
                     {/* Card1 start */}
-                    <div className="col-md-4 border rounded">
-                        <div className="container mt-2 mb-2">
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
                             <div className="row d-flex align-items-center">
                                 {/* Card1 Image */}
                                 <div className="col-md-4 d-flex justify-content-center align-items-center">
@@ -25,21 +35,181 @@ const Home = () => {
                                 {/* Card1 info */}
                                 <div className="col-md-8 mt-2">
                                     <p className="Card__Title">Your Orders</p>
-                                    <p>Track, return, or buy things again</p>
+                                    <p className="Card__Description">Track, return, or buy things again</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     {/* Card1 End */}
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
-                    <div className="col-md-4"></div>
+
+                    {/* Card2 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image2} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Login and security</p>
+                                    <p className="Card__Description">Edit login, name, and mobile number</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card2 End */}
+
+                    {/* Card3 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image3} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Prime</p>
+                                    <p className="Card__Description">View benefits and payment settings</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card3 End */}
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
+
+                    {/* Card4 start */}
+                    <div className="col-md-4 mb-4">
+                        <div className="container pt-2 pb-2 border rounded">
+                            <div className="row d-flex align-items-center">
+                                {/* Card1 Image */}
+                                <div className="col-md-4 d-flex justify-content-center align-items-center">
+                                    <img className="Card__Images" src={Image4} alt="" />
+                                </div>
+                                {/* Card1 info */}
+                                <div className="col-md-8 mt-2">
+                                    <p className="Card__Title">Gift cards</p>
+                                    <p className="Card__Description">View balance, redeem, or reload cards</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Card4 End */}
+
                 </div>
             </main>
             {/* Main part End */}
