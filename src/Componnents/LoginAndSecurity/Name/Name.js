6538c96c554a5ba77d172661393364db5da84b4d
unknown <iarafat386@gmail.com>
2021-05-24 12:16:28 +0600

    Name page complete

--- src/Componnents/LoginAndSecurity/Name/Name.js
+++ src/Componnents/LoginAndSecurity/Name/Name.js
@@ -23,6 +23,28 @@ const Name = () => {
                 <p className="Nav__Title">Change your name</p>
             </nav>
             {/* Navigation End */}
+
+            {/* Heading */}
+            <p className="Heading">Change your name</p>
+
+            {/* Main body start */}
+            <section className="container border">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 mt-2">
+                            <p>If you want to change the name associated with your Amazon customer account, you may do so below. Be sure to click the <b> Save Changes </b> button when you are done.</p>
+                        </div>
+                        <div className="col-md-3 mt-3 mb-3">
+                            <p className="Input__Title"><b>New name</b></p>
+                            <input type="text" className="Input" name="name" id="name" placeholder="Imtious Islam" />
+                        </div>
+                        <div className="col-md-12 mb-4">
+                            <button className="Button__2">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* Main body end */}
         </section>
     );
 };
